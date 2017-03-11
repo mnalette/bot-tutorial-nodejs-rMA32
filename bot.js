a4cef2ccfe515a8503d6228e2efeac7f7ea42749
mnalette <mdn1002@wildcats.unh.edu>
2017-03-11 15:12:33 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -76,7 +76,7 @@ function respond() {
   else if(request.text && botRegexDL.test(request.text)) {
     this.res.writeHead(200);
     //postMessage("http://www.daddyleagues.com/maddenrating?name=&position=all&team="+request.text.substring(5,8));
-    postMessage("http://daddyleagues.com/rcfm2/team"+request.text.substring(5,8)+"/depthchart");
+    postMessage("http://daddyleagues.com/rcfm2/teams/"+request.text.substring(5,8)+"/depthchart");
     this.res.end();
   } 
   
@@ -108,14 +108,14 @@ function respond() {
   else if(request.text && botRegexSC.test(request.text)) {
     this.res.writeHead(200);
     
-    postMessage("http://daddyleagues.com/rcfm2/team"+request.text.substring(5,8)+"/schedule");
+    postMessage("http://daddyleagues.com/rcfm2/teams/"+request.text.substring(5,8)+"/schedule");
     this.res.end();
   }
   else if(request.text && botRegexP.test(request.text)) {
     this.res.writeHead(200);
     var req = request.text.substring(5,request.text.length);
     var rep = req.replace(/ /,"+");
-    postMessage("http://daddyleagues.com/rcfm2/players");
+    postMessage("http://daddyleagues.com/rcfm2/players/");
     
     this.res.end();
   }  
