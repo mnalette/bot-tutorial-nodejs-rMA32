98e22439c93c7ce00af32ab5fc36c9a1f0d22b09
mnalette <mdn1002@wildcats.unh.edu>
2017-09-13 14:37:32 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -76,13 +76,13 @@ function respond() {
   else if(request.text && botRegexDL.test(request.text)) {
     this.res.writeHead(200);
     //postMessage("http://www.daddyleagues.com/maddenrating?name=&position=all&team="+request.text.substring(5,8));
-    postMessage("http://daddyleagues.com/rcfm2/team/"+request.text.substring(5,8)+"/depthchart");
+    postMessage("http://daddyleagues.com/rcfm/team/"+request.text.substring(5,8)+"/depthchart");
     this.res.end();
   } 
   
   else if(request.text && botRegexOW.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("www.daddyleagues.com/rcfm2/maddenrating/");
+    postMessage("www.daddyleagues.com/rcfm/maddenrating/");
     this.res.end();
   } 
   else if(request.text && botRegexSalt.test(request.text)) {
@@ -97,7 +97,7 @@ function respond() {
   }
   else if(request.text && botRegexRules.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://daddyleagues.com/rcfm2/rules");
+    postMessage("http://daddyleagues.com/rcfm/rules");
     this.res.end();
   } 
   else if(request.text && botRegexGTA.test(request.text)) {
@@ -108,14 +108,14 @@ function respond() {
   else if(request.text && botRegexSC.test(request.text)) {
     this.res.writeHead(200);
     
-    postMessage("http://daddyleagues.com/rcfm2/team/"+request.text.substring(5,8)+"/schedule");
+    postMessage("http://daddyleagues.com/rcfm/team/"+request.text.substring(5,8)+"/schedule");
     this.res.end();
   }
   else if(request.text && botRegexP.test(request.text)) {
     this.res.writeHead(200);
     var req = request.text.substring(5,request.text.length);
     var rep = req.replace(/ /,"+");
-    postMessage("http://daddyleagues.com/rcfm2/players/");
+    postMessage("http://daddyleagues.com/rcfm/players/");
     
     this.res.end();
   }  
